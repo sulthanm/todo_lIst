e7d28333f40e871da4ffa3ed573afc08e9b112d6
sulthanm <67912363+sulthanm@users.noreply.github.com>
2021-10-22 18:03:17 +0530

    modification of mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/todo-list', {useNewUrlParser: true});
 
-const MONGODB_URI = "mongodb+srv://sulthanmogal:Sulthan7866129@cluster0.0yfoq.mongodb.net/todo?"
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo-list',
     { 
         useNewUrlParser: true ,
